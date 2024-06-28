7850f1c42d57478d42d60e35576b234a48820b10
davodius <davidhanebrant@gmail.com>
2024-06-28 19:07:44 +0200

    made counter for repeating items, might be in use

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 //console.log("You have your backpack in hand \nWhat is your choice?");
+// npm install prompt-sync - kom ihåg att denna var tvungen att installeras för att följande stycke skulle fungera.
+
+/* const prompt = require("prompt-sync")();
+
+let count = 0;
+while (true) {
+  console.log("press a letter ");
+  const choice = prompt();
+  count++;
+
+  console.log(`you have pressed a letter, ${count} times`);
+}
+ */
