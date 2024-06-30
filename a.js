f45fafe54abe4d0853ad5f6da83478265b2a2815
davodius <davidhanebrant@gmail.com>
2024-06-30 22:29:18 +0200

    a.js is ready for testing

--- a.js
+++ a.js
@@ -8,10 +8,59 @@ while (true) {
   console.log("what are you adding? ");
   let thingPutInBag = prompt();
 
-  stuffInBag.push(thingPutInBag);
+  if (isNan(thingPutInBag)) {
+    //run prog
+    stuffInBag.push(thingPutInBag);
+    console.log(`added ${thingPutInBag} to the backpack.`);
+    console.log(`the bag now contains the following: ${stuffInBag}`);
+    console.log("----------------");
+
+    console.log(
+      "press [enter] to add a new item\n----------------\nenter [m] to go back to the menu\n----------------\nenter [c] for closing the backpack "
+    );
+
+    let afterAddedToArrayChoice = prompt();
+
+    if (afterAddedToArrayChoice === "c") {
+      console.log("You've closed the backpack.\n================ "),
+        process.exit();
+    } else if (afterAddedToArrayChoice === "") {
+      console.log("================ ");
+    } else if (afterAddedToArrayChoice === "m") {
+      console.log("================ ");
+      // put break here to close the adding function so the loop take back to menu -- rember
+    }
+  } else {
+    console.log(
+      "you cant put a number in a bag you silly goose\n---------------- "
+    );
+
+    console.log(
+      "press [enter] to add a new item\n----------------\nenter [m] to go back to the menu\n----------------\nenter [c] for closing the backpack "
+    );
+
+    let afterAddedToArrayChoice = prompt();
+
+    if (afterAddedToArrayChoice === "c") {
+      console.log("You've closed the backpack.\n================ "),
+        process.exit();
+    } else if (afterAddedToArrayChoice === "") {
+      console.log("================ ");
+    } else if (afterAddedToArrayChoice === "m") {
+      console.log("================ ");
+      // put break here to close the adding function so the loop take back to menu -- rember
+    }
+  } // here the silly goose gib menu retry item adding ends (above).
+
+  // this was the prog that got moved into the if in the beginning(below)
+  /* stuffInBag.push(thingPutInBag);
   console.log(`added ${thingPutInBag} to the backpack.`);
   console.log(`the bag now contains the following: ${stuffInBag}`);
   console.log("----------------");
+
+  console.log(
+    "press [enter] to add a new item\n----------------\nenter [m] to go back to the menu\n----------------\nenter [c] for closing the backpack "
+  );
 
   let afterAddedToArrayChoice = prompt();
 
@@ -22,12 +71,12 @@ while (true) {
     console.log("================ ");
   } else if (afterAddedToArrayChoice === "m") {
     console.log("================ ");
-    // put break here
-  }
+    // put break here to close the adding function so the loop take back to menu -- rember
+  } */
 
   // kom ihåg att fixa: bara bokstäver skall tas emot/ inga siffror
   //+ kunna lägga till hur många man vill (loopa, tror while true blir bra). - klar
   // stänga väskan för att klar - klar
-  // gå tillbaka till menyn - prova med breaken sen.
+  // gå tillbaka till menyn - prova med breaken sen. <====== kom ihåg!!
   // lägga till en ny - klar
 }
