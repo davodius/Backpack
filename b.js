56dc07a07299737b0668ca0ec5c50bc77a36efe6
davodius <davidhanebrant@gmail.com>
2024-06-30 09:44:48 +0200

    try to get return to index choice menu if input != number + higher than biggest index

--- b.js
+++ b.js
@@ -11,32 +11,34 @@ while (true) {
     console.log(`${index + 1}: ${element}`);
     console.log("----------------");
   });
-  let removeItemFromBag = prompt();
+  while (true) {
+    let removeItemFromBag = prompt();
 
-  let highestIndex = stuffInBag.length - 1;
+    let highestIndex = stuffInBag.length - 1;
+    if (removeItemFromBag >= 1) {
+      // Convert user input to zero-based index
+      let removeIndex = removeItemFromBag - 1;
 
-  if (removeItemFromBag >= 1) {
-    // Convert user input to zero-based index
-    let removeIndex = removeItemFromBag - 1;
+      if (removeIndex >= 0 && removeIndex < stuffInBag.length) {
+        stuffInBag.splice(removeIndex, 1);
+      }
 
-    if (removeIndex >= 0 && removeIndex < stuffInBag.length) {
-      stuffInBag.splice(removeIndex, 1);
+      stuffInBag.forEach((element, index) => {
+        console.log(`${index + 1}: ${element}`);
+      });
     }
-
-    stuffInBag.forEach((element, index) => {
-      console.log(`${index + 1}: ${element}`);
-    });
+    if (removeItemFromBag > stuffInBag.length - 1) {
+      console.log("that id does not exist ");
+      //console.log(highestIndexValue); // Output: 50
+    } else {
+      console.log("it needs to be a number ");
+    }
   }
-  if (removeItemFromBag > stuffInBag.length - 1) {
-    console.log("that id does not exist ");
-    //console.log(highestIndexValue); // Output: 50
-  }
-
   console.log("write [m] to go to the menu and [c] to close the backpack ");
 
   let menuReturnChoice = prompt();
 
-  menuReturnChoice == "n"
+  menuReturnChoice == "c"
     ? (console.log("You've closed the backpack."), process.exit())
     : console.log("================");
 }
