f098f2b949c80cbda3a49ef01ba276c4dc93472e
davodius <davidhanebrant@gmail.com>
2024-06-30 14:21:00 +0200

    moved working case two from separate program b bc it worked there.

--- index.js
+++ index.js
@@ -32,39 +32,58 @@ switch (choice) {
 
   case "2":
     while (true) {
-      console.log("select the id of what you wish to remove: ");
-      stuffInBag.forEach((element, index) => {
-        console.log(`${index + 1}: ${element}`);
-        console.log("----------------");
-      });
-      let removeItemFromBag = prompt();
+      if (stuffInBag.length === 0) {
+        console.log(
+          "the backpack is empty\npress [enter] to close it\npress [m] to return to menu "
+        );
+        let arrayIsEmptychoice = prompt();
 
-      if (removeItemFromBag >= 1) {
-        let removeIndex = removeItemFromBag - 1;
-
-        if (removeIndex >= 0 && removeIndex < stuffInBag.length) {
-          stuffInBag.splice(removeIndex, 1);
-        }
+        arrayIsEmptychoice == "m"
+          ? console.log("================")
+          : (console.log("You've closed the backpack."), process.exit());
+      } else {
+        console.log("select the id of what you wish to remove: ");
 
         stuffInBag.forEach((element, index) => {
           console.log(`${index + 1}: ${element}`);
           console.log("----------------");
         });
-      } else {
+        let removeItemFromBag = prompt();
+
+        let highestIndex = stuffInBag.length - 1;
+        if (removeItemFromBag >= 1) {
+          // Convert user input to zero-based index
+          let removeIndex = removeItemFromBag - 1;
+
+          if (removeIndex >= 0 && removeIndex < stuffInBag.length) {
+            stuffInBag.splice(removeIndex, 1);
+          }
+
+          stuffInBag.forEach((element, index) => {
+            console.log(`${index + 1}: ${element}`);
+          });
+        }
+        if (removeItemFromBag > stuffInBag.length - 1) {
+          console.log("that id does not exist ");
+          //console.log(highestIndexValue);
+        } else if (isNaN(removeItemFromBag)) {
+          console.log(
+            `${removeItemFromBag} is not a number\n----------------\nit needs to be a number\n---------------- `
+          );
+        }
+
         console.log(
-          "you need to press an index number that is more or equal to 1 "
+          "press [enter] to try a new index\n----------------\nenter [m] to go back to the menu\n----------------\nenter [c] for closing the backpack "
         );
+
+        let menuReturnChoice = prompt();
+
+        menuReturnChoice == "m"
+          ? "================"
+          : menuReturnChoice == "c"
+          ? (console.log("You've closed the backpack."), process.exit())
+          : console.log("================");
       }
-
-      console.log(
-        "write [y] to go back to the menu and [n] to close the backpack "
-      );
-
-      let menuReturnChoice = prompt();
-
-      menuReturnChoice == "n"
-        ? (console.log("You've closed the backpack."), process.exit())
-        : console.log("================");
 
       break;
     }
